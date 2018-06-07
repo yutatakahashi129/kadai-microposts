d8e032641afeaba7fd087b2928c18957bdea9da5
Yuta Takahashi <u.ta4263@gmail.com>
2018-06-07 01:33:31 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,8 +11,8 @@
                 <a class="navbar-brand" href="/">Microposts</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Signup</a></li>
+                 <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
                     <li><a href="#">Login</a></li>
                 </ul>
             </div>
